04dafb2d1daeda0834e0175665a2915363cc0812
unagimamire <garden5149@gmail.com>
2021-11-07 12:35:49 +0900

    ooooo

---
+++ src/main/java/sample/Game001Application.java
@@ -0,0 +1,13 @@
+package sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Game001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Game001Application.class, args);
+	}
+
+}
---
+++ src/test/java/sample/Game001ApplicationTests.java
@@ -0,0 +1,13 @@
+package sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Game001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
